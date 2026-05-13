63f2904aa350c32b79e3aee76a30bc0c0c1d852f
pranavkrishna18 <pranavkrishna2796@gmail.com>
2026-05-13 22:35:23 +0530

    Fix formatting in AccordionItem component

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -9,7 +9,7 @@ const Accordion = AccordionPrimitive.Root;
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->(({ className, ...props }, ref) => (
+>(({ className, ...props }, ref) => ( 
   <AccordionPrimitive.Item ref={ref} className={cn("border-b", className)} {...props} />
 ));
 AccordionItem.displayName = "AccordionItem";
